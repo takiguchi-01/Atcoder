accf7c9ed1e9af7249e5c0d5e4e13484945eaaa8
mrbks1102 <[mrbks1102@gmail.com]>
2021-01-19 18:41:28 +0900

    ABC186_A

--- field/contests/abc187/a.py
+++ field/contests/abc187/a.py
@@ -1,6 +1,4 @@
-a,b = map(list,input().split())
-a = sum(map(int,a))
-b = sum(map(int,b))
-print(max(a,b))
-
-
+a, b = map(list, input().split())
+a = sum(map(int, a))
+b = sum(map(int, b))
+print(max(a, b))

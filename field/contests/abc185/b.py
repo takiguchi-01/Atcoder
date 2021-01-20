d09a4164dbfa6965e883af87707ca9300be6e19e
mrbks1102 <[mrbks1102@gmail.com]>
2021-01-20 19:05:47 +0900

    ABC185_B

---
+++ field/contests/abc185/b.py
@@ -0,0 +1,18 @@
+n, m, t = map(int, input().split())
+now = 0
+time = n
+
+for i in range(m):
+    a, b = map(int, input().split())
+    time -= a-now
+    if time <= 0:
+        print("No")
+        exit()
+    time += b-a
+    time = min(time, n)
+    now = b
+time -= t-now
+if time <= 0:
+    print("No")
+else:
+    print("Yes")
